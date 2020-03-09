fc554854441d785f3e204f2b2165bf398786953f
Humberto Dias <humberto.dias@ubisoft.com>
2020-03-09 01:15:15 +0100

    Adding dynamic load of assets

--- Assets/Scripts/CalcHotfix.cs
+++ Assets/Scripts/CalcHotfix.cs
@@ -1,21 +1,80 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using System.IO;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 using XLua;
 
 public class CalcHotfix : MonoBehaviour
 {
+    /*
     void Start()
     {
         Debug.Log("Start hotfix");
         LuaEnv luaenv = new LuaEnv();
-        /*        luaenv.DoString(@"
-                    xlua.hotfix(CS.XLuaTest.CalcService,'Add',function(self, a, b)
-                        print('LOL')
-                        return a * b
-                    end)");
-                    */
+        luaenv.DoString("require 'calc'");
+    }*/
+
+    public InputField serverAssetBundle;
+    private AssetBundle assetBundle;
+
+    // Start is called before the first frame update
+    public void ApplyLuaFix()
+    {
+        Debug.Log("DownLoad Script");
+        // Start coroutine
+        StartCoroutine(GetAssetBundle(ExcuteHotFix));
+    }
+    //-----------------------------【Download hot-resources from the server】-----------------------------
+    IEnumerator GetAssetBundle(Action callBack)
+    {
+        string path = serverAssetBundle.text;
+        Debug.Log("DownLoading: " + path);
+        UnityWebRequest www = UnityWebRequestAssetBundle.GetAssetBundle(path);
+        yield return www.SendWebRequest();
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log("DownLoad Err: " + www.error);
+        }
+        else
+        {
+            assetBundle = DownloadHandlerAssetBundle.GetContent(www);
+            //Load downloaded lua script
+            TextAsset hot = assetBundle.LoadAsset<TextAsset>("calc.lua.txt");
+            //Save to local folder
+            string newPath = Application.persistentDataPath + @"/calc.lua.txt";
+            if (!File.Exists(newPath))
+            {
+                // If you do not actively release resources after Create, it will be occupied, and an error will be reported next time you open it, so you must add .Dispose ()
+                File.Create(newPath).Dispose();
+            }
+            File.WriteAllText(newPath, hot.text);
+            Debug.Log("Download resource succeeded！new Path : " + newPath);
+            // After successful download, read and execute lua script
+            callBack();
+        }
+    }
+
+    //-----------------------------【Execute hot change script】-----------------------------
+    public void ExcuteHotFix()
+    {
+        Debug.Log("Start executing hot change script calc");
+        LuaEnv luaenv = new LuaEnv();
+        luaenv.AddLoader(MyLoader);
         luaenv.DoString("require 'calc'");
     }
+
+    // Custom Loader
+    public byte[] MyLoader(ref string filePath)
+    {
+        // Reading downloaded script resources
+        string newPath = Application.persistentDataPath + @"/" + filePath + ".lua.txt";
+        Debug.Log("Execute script path：" + newPath);
+        string txtString = File.ReadAllText(newPath);
+        return System.Text.Encoding.UTF8.GetBytes(txtString);
+    }
+
+
 
 }
---
+++ Assets/Scripts/Editor/AssetBundleCreator.cs
@@ -0,0 +1,23 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class AssetBundleCreator : MonoBehaviour {
+
+    [MenuItem("Assets/Build Asset Bundle")]
+    static void BuildBuildes()
+    {
+        string dir = "AssetBundles";
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+        var options = BuildAssetBundleOptions.None;
+//        var target = BuildTarget.StandaloneLinux64;
+        var target = BuildTarget.Android;
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles", options, target);
+        Debug.Log("AssetBudles generated");
+    }
+}
+#endif
